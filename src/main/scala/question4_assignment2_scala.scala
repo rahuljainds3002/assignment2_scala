8fd350307ace9267a860dbe213d8830bc5487dc0
rahul jain <rahul.jainds3002@gmail.com>
2024-06-29 21:20:18 +0530

    Quest4_assignment2_scala

---
+++ src/main/scala/question4_assignment2_scala.scala
@@ -0,0 +1,11 @@
+object question4_assignment2_scala {
+  def main(args: Array[String]): Unit = {
+    var i = 0
+    while (i < 5) {
+      var a = i*i
+        println(s"the  number $a is and even number")
+
+      i += 1
+    }
+  }
+}
