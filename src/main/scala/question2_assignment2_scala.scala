e1ca612bf1ed57d19e7ab69b8b790fa1629c9821
rahul jain <rahul.jainds3002@gmail.com>
2024-06-29 21:12:38 +0530

    Quest2_assignment2_scala

---
+++ src/main/scala/question2_assignment2_scala.scala
@@ -0,0 +1,13 @@
+object question2_assignment2_scala {
+  def main(args: Array[String]): Unit = {
+    var sum = 0
+    for(i<-1 to 50){
+
+      sum +=i
+      //println(s"the total number is $sum" )
+    }
+    println(s"the total number is $sum" )
+
+  }
+
+}
