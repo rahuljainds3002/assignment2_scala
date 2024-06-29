85062fb5bbffafe2a4f8b1fc7c8a73c39aef1c67
rahul jain <rahul.jainds3002@gmail.com>
2024-06-29 21:33:44 +0530

    Quest6_assignment2_scala

--- src/main/scala/Questin6_assignment2_scala.scala
+++ src/main/scala/Questin6_assignment2_scala.scala
@@ -1,11 +1,10 @@
 object Questin6_assignment2_scala {
   def main(args: Array[String]): Unit = {
-    var i = 1
-    while (i < 6) {
+
+    for(i<-1 until 6) {
       var a = 3*i
       println(s"the  number $a is and even number")
 
-      i += 1
     }
   }
 
