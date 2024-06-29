004c9554f778d667668ae2b5e330ba3599c49b0d
rahul jain <rahul.jainds3002@gmail.com>
2024-06-29 21:09:35 +0530

    Quest1_assignment2_scala

--- src/main/scala/question1_assignment2_scala.scala
+++ src/main/scala/question1_assignment2_scala.scala
@@ -1,17 +1,8 @@
 object question1_assignment2_scala {
   def main(args: Array[String]): Unit = {
-    var amount = 190
-    var loyalty_card = "No"
-    if (amount >= 200 || loyalty_card == "true") {
-      println("person is a eligible for discount")
-    }
-    else if (amount < 200 && loyalty_card == "true") {
-      println("person is eligible for discount")
-    }
-
-    else {
-      println("customer is not eligible for discount")
-    }
+   for(i<-1 to 5){
+     println(s"the number is $i" )
+   }
   }
 
   }
