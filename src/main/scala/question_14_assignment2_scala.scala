5c0d355a9d5c2dd589b59d308598cc7dbdda2341
rahul jain <rahul.jainds3002@gmail.com>
2024-06-30 19:42:07 +0530

    Quest_14_assignment2_scala

---
+++ src/main/scala/question_14_assignment2_scala.scala
@@ -0,0 +1,18 @@
+object question_14_assignment2_scala {
+
+    def main(args: Array[String]): Unit =
+    {
+      var i = 1
+      while(i<4){
+        var b = i*i*i
+        println(s"cube of number $i is $b")
+        i+=1}
+
+    }
+  }
+
+
+
+
+
+
